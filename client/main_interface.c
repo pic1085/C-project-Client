7489cdd82fd3af386875a271d4e7a37b2c7c4117
pic1085 <incheol1085@gmail.com>
2024-08-06 20:58:57 +0900

    feat : main interface 출력

---
+++ client/main_interface.c
@@ -0,0 +1,19 @@
+#include "main_interface.h"
+
+void Main_Interface() { // 메인 인터페이스 
+    printf("+=======================================================+\n");
+    printf("|                  <FileSharing System>                 |\n");
+    printf("|                  - Select a Service -                 |\n");
+    printf("|                                                       |\n");
+    printf("|             >>> 1. File upload                        |\n");
+    printf("|                                                       |\n");
+    printf("|             >>> 2. File download                      |\n");
+    printf("|                                                       |\n");
+    printf("|             >>> 3. File/User List                     |\n");
+    printf("|                                                       |\n");
+    printf("|             >>> 4. Exit                               |\n");
+    printf("|                                                       |\n");
+    printf("+=======================================================+\n");
+    printf("| Input : upload, download, userlist, filelist, exit    |\n");
+    printf("+=======================================================+\n");
+}
---
+++ client/main_interface.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_INTERFACE_H
+#define MAIN_INTERFACE_H
+
+#include "server.h" 
+
+void Main_Interface();
+
+#endif
