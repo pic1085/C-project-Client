18f64d1f8ac98a6301d63ff057c2dccdc7e03a88
pic1085 <incheol1085@gmail.com>
2024-08-06 21:01:16 +0900

    feat : 로그인을 위한 인터페이스

---
+++ client/user_interface.c
@@ -0,0 +1,17 @@
+#include "user_interface.h"
+
+void User_Interface() { // 로그인 인터페이스 
+    printf("+==============================+\n");
+    printf("|     <FileSharing System>     |\n");
+    printf("|     - Select a Service -     |\n");
+    printf("|                              |\n");
+    printf("|  >>> 1. Log in.              |\n");
+    printf("|                              |\n");
+    printf("|  >>> 2. Join Membership.     |\n");
+    printf("|                              |\n");
+    printf("|  >>> 3. Exit.                |\n");
+    printf("|                              |\n");
+    printf("+==============================+\n");
+    printf("| Input : login, join, exit    |\n");
+    printf("+==============================+\n");
+}
---
+++ client/user_interface.h
@@ -0,0 +1,8 @@
+#ifndef USER_INTERFACE_H
+#define USER_INTERFACE_H
+
+#include "server.h"
+
+void User_Interface();
+
+#endif
